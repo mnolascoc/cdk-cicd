c3cec87835477abb40074544352845bfc1c93431
Miguel Nolasco <mnolascoc@outlook.com>
2023-08-19 12:31:30 -0500

    se agrega lambdastack

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,17 +1,24 @@
 import * as cdk from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineStage } from "./pipeline-stage";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, "MyPipeline", {
+    const pipeline = new CodePipeline(this, "MyPipeline", {
       pipelineName: "MyPipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("mnolascoc/cdk-cicd", "cicd-practice"),
         commands: ["npm ci", "npx cdk synth"],
       }),
     });
+
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      })
+    );
   }
 }
