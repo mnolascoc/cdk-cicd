cd98e5b1f717c3b4517227a334571ff98433409b
Miguel Nolasco <mnolascoc@outlook.com>
2023-08-19 06:50:34 -0500

    cambio

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,17 @@
 import * as cdk from "aws-cdk-lib";
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, "MyPipeline", {
+      pipelineName: "MyPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("mnolascoc/cdk-cicd", "cicd-practice"),
+        commands: ["npm ci", "npx cdk synth"],
+      }),
+    });
   }
 }
